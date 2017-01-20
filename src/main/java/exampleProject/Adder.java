29f75d0a4f89a33781815be004f6cb9e01c7dcf8
Gordon <JGordon@corp.group.local>
2017-01-20 16:36:55 +0000

    Added new adding feature

---
+++ src/main/java/exampleProject/Adder.java
@@ -0,0 +1,25 @@
+package exampleProject;
+
+public class Adder {
+
+	private int value1;
+	private int value2;
+
+	public Adder(int val1, int val2) {
+		this.value1 = val1;
+		this.value2 = val2;
+	}
+
+	public int getTotal() {
+		return getValue1() + getValue2();
+	}
+
+	public int getValue1() {
+		return value1;
+	}
+
+	public int getValue2() {
+		return value2;
+	}
+
+}
