306f784b272a5931a375895bd885c7c760af5d54
Colin Heyl <oncilelhy@googlemail.com>
2017-01-23 15:14:57 +0000

    Update to MedTask
    Removed unnecessary constructor

--- src/main/MedTask.java
+++ src/main/MedTask.java
@@ -1,20 +1,17 @@
 package tests;
 
-public class MedTask {
-
-	public MedTask(){
-		
-	}
-	
-	public int[] arrayReverse(int[] newArray) {
+public class MedTask
+{	
+	public int[] arrayReverse(int[] newArray)
+	{
 		int[] tempArray = new int[4];
 		int locToChange = newArray.length -1;
 		
-		for (int i=0; i<newArray.length; i++){
+		for (int i=0; i<newArray.length; i++)
+		{
 			tempArray[i] = newArray[locToChange];			
 			locToChange--;
 		}
-		
 		return tempArray;
 	}
 }
