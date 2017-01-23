4575af210d0b3e4847ff96f7d907253c5dc9409f
lameay <l.hoang@my.westminster.ac.uk>
2017-01-23 14:27:53 +0000

    Medium Task - Complete

--- src/test/java/exampleProject/ReverseTest.java
+++ src/test/java/exampleProject/ReverseTest.java
@@ -9,15 +9,15 @@ public class ReverseTest {
 	@Test
 	public void reverseTest() {
 		Reverser rev = new Reverser();
-		
-		int[] nameOfArray = {2,4,5,7,8,4};
-		
+
+		int[] nameOfArray = { 2, 4, 5, 7, 8, 4 };
+
 		int[] reversedArray = rev.reverse(nameOfArray);
-		
-		int[] actualReversedArray = {4, 8, 7, 5, 4, 2};
-		
+
+		int[] actualReversedArray = { 4, 8, 7, 5, 4, 2 };
+
 		assertArrayEquals(reversedArray, actualReversedArray);
-		
+
 	}
-	
+
 }
